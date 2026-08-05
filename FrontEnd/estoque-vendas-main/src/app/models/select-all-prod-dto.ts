115fa723fd5a73d821a74badd4fb31adda81dea5
Gabriel Souza <gabrielocampeao@hotmail.com>
2026-08-05 19:47:48 -0300

    feat: Adiciona novas telas de cadastro de produto e atualização de dados do mesmo

---
+++ FrontEnd/estoque-vendas-main/src/app/models/select-all-prod-dto.ts
@@ -0,0 +1,15 @@
+export interface SelectAllProdDTO {
+  prodId: string;
+
+  nome: string;
+
+  codBarra: string;
+
+  quantidade: number;
+
+  precoCusto: number;
+
+  precoVenda: number;
+  
+  prodStatus: string;
+}
