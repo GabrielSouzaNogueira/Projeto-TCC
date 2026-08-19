280afabf8191d11fad7a793ea72f23f66dcd7ace
Gabriel Souza <gabrielocampeao@hotmail.com>
2026-08-18 23:45:12 -0300

    feat(MovController): adiciona o endpoint de filtro para ser listado as movimentações

--- backEnd/gerenciadorEstoque/src/main/java/by/gabriel/gerenciadorEstoque/Api/Controller/MovController.java
+++ backEnd/gerenciadorEstoque/src/main/java/by/gabriel/gerenciadorEstoque/Api/Controller/MovController.java
@@ -1,12 +1,16 @@
 package by.gabriel.gerenciadorEstoque.Api.Controller;
 
 import by.gabriel.gerenciadorEstoque.Api.DTO.Movimentacao.MovUserDTO;
+import by.gabriel.gerenciadorEstoque.Enum.Movimentacao.Usuario.MovUserAcao;
 import by.gabriel.gerenciadorEstoque.Services.MovUserService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -20,10 +24,20 @@ public class MovController {
     }
 
     @GetMapping("/listAllMov")
-    public ResponseEntity<List<MovUserDTO>> listAllMov() { // Trocado de FilterSelectMovUser para MovUserDTO
-
+    public ResponseEntity<List<MovUserDTO>> listAllMov() {
         List<MovUserDTO> listados = movUserService.listAllMov();
+        return ResponseEntity.ok(listados);
+    }
 
+    @GetMapping("/filtrar")
+    public ResponseEntity<List<MovUserDTO>> filtrarMovimentacoes(
+            @RequestParam(required = false) MovUserAcao acao,
+            @RequestParam(required = false) String responsavel,
+            @RequestParam(required = false) String usuarioAfetado,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataInicio,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFim
+    ) {
+        List<MovUserDTO> listados = movUserService.filtrarMovimentacoes(acao, responsavel, usuarioAfetado, dataInicio, dataFim);
         return ResponseEntity.ok(listados);
     }
 }
