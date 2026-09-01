4542895cdd07cc57cf4457436cf8cf84a8bfe4d6
Gabriel Souza <gabrielocampeao@hotmail.com>
2026-08-31 22:53:38 -0300

    fix: Ajusta erro de caminho do arquivo record da consulta dos clientes

--- backEnd/gerenciadorEstoque/src/main/java/by/gabriel/gerenciadorEstoque/Repository/Cliente/ClienteRepository.java
+++ backEnd/gerenciadorEstoque/src/main/java/by/gabriel/gerenciadorEstoque/Repository/Cliente/ClienteRepository.java
@@ -1,7 +1,6 @@
 package by.gabriel.gerenciadorEstoque.Repository.Cliente;
 
 import by.gabriel.gerenciadorEstoque.Api.DTO.Cliente.Consultas.ClienteSelectDTO;
-import by.gabriel.gerenciadorEstoque.Api.DTO.Cliente.Consultas.ClienteSelectDTO;
 import by.gabriel.gerenciadorEstoque.Enum.Cliente.ClienteStatus;
 import by.gabriel.gerenciadorEstoque.Model.Cliente.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,7 +16,8 @@ public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
     Optional<Cliente> findByEmailIgnoreCase(String email);
 
-    @Query("SELECT new by.gabriel.gerenciadorEstoque.Api.DTO.Cliente.Consultas(" +
+    // O caminho agora inclui o pacote .Consultas.
+    @Query("SELECT new by.gabriel.gerenciadorEstoque.Api.DTO.Cliente.Consultas.ClienteSelectDTO(" +
             "c.id, c.nome, c.sobrenome, c.email, c.telefone, c.status) " +
             "FROM Cliente c WHERE c.status = :status " +
             "ORDER BY c.nome ASC")
